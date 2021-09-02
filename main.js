c1c3fc640f2b5c2458967ded5a4a022b95e6a276
asif247 <86229363+asif247@users.noreply.github.com>
2021-09-02 15:56:39 +0600

    https

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const searchBook = () => {
         searchText = searchField.value
         //console.log(searchText)
         searchField.value = ''
-        const url = `http://openlibrary.org/search.json?q=${searchText}`
+        const url = `https://openlibrary.org/search.json?q=${searchText}`
         fetch(url)
         .then(response => response.json())
         .then(data => {
