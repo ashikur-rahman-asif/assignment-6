b62cffa96a0df17a2f79c641575f51ce57f5d17b
asif247 <86229363+asif247@users.noreply.github.com>
2021-09-02 23:02:01 +0600

    keyword

--- main.js
+++ main.js
@@ -35,10 +35,10 @@ const displaySearchResult = (books) => {
         let actualName = (nameStr.split(",")[0]);
         let publishOn = String(book.publish_date)
         singleItem += `
-        <div class="item">
-        <div class="image-wrapper">
-        <img src="${book.cover_i?`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`:`https://image.shutterstock.com/image-vector/no-image-available-vector-illustration-260nw-744886198.jpg`}" alt="">
-        </div>
+                <div class="item">
+                    <div class="image-wrapper">
+                        <img src="${book.cover_i?`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`:`https://image.shutterstock.com/image-vector/no-image-available-vector-illustration-260nw-744886198.jpg`}" alt="">
+                    </div>
                     <div class="flex-container">
                         <h1 class="title">Title: ${book.title?book.title:"Title Not Found"}</h1>
                     </div>
@@ -46,7 +46,7 @@ const displaySearchResult = (books) => {
                     <p class="item-data">Published on: ${publishOn==='undefined'?"Publish Date Not Found":publishOn}</p>
                     <p class="item-data">First publish: ${book.first_publish_year?book.first_publish_year:"Date Not Found"}</p>
                     <p class="item-data">Publisher: ${book.publisher?book.publisher:"Publisher Not Found"}</p>
-        </div>
+                 </div>
         `
     })
     searchResult.innerHTML = singleItem
